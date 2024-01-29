b998a5fc55bd652591907d45ffb13a5a5bfd4d28
Sukanya <01fe22bca138@kletech.ac.in>
2024-01-29 20:47:32 +0530

    code to print courses and marks

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
-public class Student {
+public class Student extends StudentCourses {
     String name;
     String dob;
 
@@ -24,6 +24,7 @@ public class Student {
         s.name = "Sukanya Dharwad";
         s.displayName();
         s.displayAge("19-06-2004");
-        
+        StudentCourses studentCourses = new StudentCourses();
+        studentCourses.displayStudentMarks();
     }
 }
